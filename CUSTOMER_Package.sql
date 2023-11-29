a893903e68e8f9cd7a005281e7e4504d9980c87a
Yakgna <ramasamy.y@northeastern.edu>
2023-11-29 17:24:52 -0500

    Added CUSTOMER_Package.sql

---
+++ CUSTOMER_Package.sql
@@ -0,0 +1,208 @@
+CLEAR SCREEN;
+SET SERVEROUTPUT ON
+CREATE OR REPLACE PACKAGE customer_pkg AS 
+    
+    PROCEDURE Upsert_customer(
+        fname IN SWADKHAJANA_ADMIN.CUSTOMER_DETAILS.FIRST_NAME%TYPE, 
+        lname IN SWADKHAJANA_ADMIN.CUSTOMER_DETAILS.LAST_NAME%TYPE,
+        ph_num IN SWADKHAJANA_ADMIN.CUSTOMER_DETAILS.PHONE_NUMBER%TYPE,
+        e_id IN SWADKHAJANA_ADMIN.CUSTOMER_DETAILS.EMAIL_ID%TYPE);
+    
+    PROCEDURE Upsert_delivery_address(
+        p_address_id SWADKHAJANA_ADMIN.DELIVERY_ADDRESS.DELIVERY_ADDRESS_ID%TYPE,
+        p_cust_id SWADKHAJANA_ADMIN.DELIVERY_ADDRESS.CUSTOMER_ID%TYPE,
+        p_line_1 SWADKHAJANA_ADMIN.DELIVERY_ADDRESS.ADDRESS_LINE_1%TYPE,
+        p_line_2 SWADKHAJANA_ADMIN.DELIVERY_ADDRESS.ADDRESS_LINE_2%TYPE,
+        p_city SWADKHAJANA_ADMIN.DELIVERY_ADDRESS.CITY%TYPE,
+        p_state SWADKHAJANA_ADMIN.DELIVERY_ADDRESS.STATE%TYPE,
+        p_pincode SWADKHAJANA_ADMIN.DELIVERY_ADDRESS.PINCODE%TYPE
+    );
+  
+    PROCEDURE Upsert_billing_address(
+        p_address_id SWADKHAJANA_ADMIN.BILLING_ADDRESS.BILLING_ADDRESS_ID%TYPE,
+        p_cust_id SWADKHAJANA_ADMIN.BILLING_ADDRESS.CUSTOMER_ID%TYPE,
+        p_line_1 SWADKHAJANA_ADMIN.BILLING_ADDRESS.ADDRESS_LINE_1%TYPE,
+        p_line_2 SWADKHAJANA_ADMIN.BILLING_ADDRESS.ADDRESS_LINE_2%TYPE,
+        p_city SWADKHAJANA_ADMIN.BILLING_ADDRESS.CITY%TYPE,
+        p_state SWADKHAJANA_ADMIN.BILLING_ADDRESS.STATE%TYPE,
+        p_pincode SWADKHAJANA_ADMIN.BILLING_ADDRESS.PINCODE%TYPE
+    );
+    
+END customer_pkg;
+/
+
+CREATE OR REPLACE PACKAGE BODY customer_pkg AS
+
+PROCEDURE Upsert_customer(
+    fname IN SWADKHAJANA_ADMIN.CUSTOMER_DETAILS.FIRST_NAME%TYPE, 
+    lname IN SWADKHAJANA_ADMIN.CUSTOMER_DETAILS.LAST_NAME%TYPE,
+    ph_num IN SWADKHAJANA_ADMIN.CUSTOMER_DETAILS.PHONE_NUMBER%TYPE,
+    e_id IN SWADKHAJANA_ADMIN.CUSTOMER_DETAILS.EMAIL_ID%TYPE)
+IS
+BEGIN
+    -- Check if the customer already exists based on email_id
+    DECLARE
+        v_email_id SWADKHAJANA_ADMIN.CUSTOMER_DETAILS.EMAIL_ID%TYPE;
+    BEGIN
+        SELECT EMAIL_ID INTO v_email_id 
+        FROM SWADKHAJANA_ADMIN.CUSTOMER_DETAILS WHERE EMAIL_ID = e_id;
+
+        -- Update the existing record
+        UPDATE SWADKHAJANA_ADMIN.CUSTOMER_DETAILS
+        SET FIRST_NAME = fname,
+            LAST_NAME = lname,
+            PHONE_NUMBER = ph_num
+        WHERE EMAIL_ID = e_id;
+
+        DBMS_OUTPUT.PUT_LINE('Customer details updated');
+    EXCEPTION 
+        WHEN NO_DATA_FOUND THEN
+            BEGIN
+                -- Insert a new record if the customer doesn't exist
+                INSERT INTO SWADKHAJANA_ADMIN.CUSTOMER_DETAILS (
+                    CUSTOMER_ID, FIRST_NAME, LAST_NAME, PHONE_NUMBER, EMAIL_ID
+                ) VALUES (
+                    SWADKHAJANA_ADMIN.CUSTOMER_DETAILS_SEQ.NEXTVAL, fname, lname, ph_num, e_id
+                );
+
+                DBMS_OUTPUT.PUT_LINE('Customer added');
+            EXCEPTION 
+                WHEN DUP_VAL_ON_INDEX THEN
+                    DBMS_OUTPUT.PUT_LINE('Phone number already tagged to a different Email address');
+            END;
+        WHEN DUP_VAL_ON_INDEX THEN
+            DBMS_OUTPUT.PUT_LINE('Phone number already tagged to a different Email address');
+    END;
+    COMMIT; -- Commit the changes
+EXCEPTION
+    WHEN OTHERS THEN
+        -- Handle other exceptions or log errors as needed
+        DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
+        ROLLBACK; -- Rollback changes if an error occurs
+END Upsert_customer;
+
+PROCEDURE Upsert_delivery_address(
+    p_address_id SWADKHAJANA_ADMIN.DELIVERY_ADDRESS.DELIVERY_ADDRESS_ID%TYPE,
+    p_cust_id SWADKHAJANA_ADMIN.DELIVERY_ADDRESS.CUSTOMER_ID%TYPE,
+    p_line_1 SWADKHAJANA_ADMIN.DELIVERY_ADDRESS.ADDRESS_LINE_1%TYPE,
+    p_line_2 SWADKHAJANA_ADMIN.DELIVERY_ADDRESS.ADDRESS_LINE_2%TYPE,
+    p_city SWADKHAJANA_ADMIN.DELIVERY_ADDRESS.CITY%TYPE,
+    p_state SWADKHAJANA_ADMIN.DELIVERY_ADDRESS.STATE%TYPE,
+    p_pincode SWADKHAJANA_ADMIN.DELIVERY_ADDRESS.PINCODE%TYPE
+)
+IS
+BEGIN
+    -- Check if the customer already exists based on email_id
+    DECLARE
+    v_address_id SWADKHAJANA_ADMIN.DELIVERY_ADDRESS.Delivery_Address_Id%TYPE;
+    v_customer_id SWADKHAJANA_ADMIN.DELIVERY_ADDRESS.CUSTOMER_ID%TYPE;
+    BEGIN
+        SELECT delivery_address_id INTO v_address_id 
+        FROM SWADKHAJANA_ADMIN.DELIVERY_ADDRESS WHERE delivery_address_id = p_address_id;
+        
+        -- Update the existing record
+        UPDATE SWADKHAJANA_ADMIN.DELIVERY_ADDRESS
+        SET ADDRESS_LINE_1 = p_line_1,
+            ADDRESS_LINE_2 = p_line_2,
+            CITY = p_city,
+            STATE = p_state,
+            PINCODE = p_pincode
+        WHERE DELIVERY_ADDRESS_ID = p_address_id;
+
+        DBMS_OUTPUT.PUT_LINE('Address updated');
+    EXCEPTION 
+        WHEN NO_DATA_FOUND THEN
+            -- Insert a new record if the address id is new
+            INSERT INTO SWADKHAJANA_ADMIN.DELIVERY_ADDRESS VALUES 
+                 (p_address_id, p_cust_id, p_line_1, p_line_2,
+                    p_city, p_state, p_pincode);
+            DBMS_OUTPUT.PUT_LINE('Address added');
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
+    END;   
+    COMMIT; -- Commit the changes
+EXCEPTION
+    WHEN OTHERS THEN
+        IF SQLCODE = -2291 THEN
+            DBMS_OUTPUT.PUT_LINE('Invalid customer ID');
+        ELSE
+            -- Handle other exceptions or log errors as needed
+            DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
+        END IF;
+        ROLLBACK; -- Rollback changes if an error occurs
+END Upsert_delivery_address;
+
+PROCEDURE Upsert_billing_address(
+    p_address_id SWADKHAJANA_ADMIN.BILLING_ADDRESS.BILLING_ADDRESS_ID%TYPE,
+    p_cust_id SWADKHAJANA_ADMIN.BILLING_ADDRESS.CUSTOMER_ID%TYPE,
+    p_line_1 SWADKHAJANA_ADMIN.BILLING_ADDRESS.ADDRESS_LINE_1%TYPE,
+    p_line_2 SWADKHAJANA_ADMIN.BILLING_ADDRESS.ADDRESS_LINE_2%TYPE,
+    p_city SWADKHAJANA_ADMIN.BILLING_ADDRESS.CITY%TYPE,
+    p_state SWADKHAJANA_ADMIN.BILLING_ADDRESS.STATE%TYPE,
+    p_pincode SWADKHAJANA_ADMIN.BILLING_ADDRESS.PINCODE%TYPE
+)
+IS
+BEGIN
+    -- Check if the customer already exists based on email_id
+    DECLARE
+    v_address_id SWADKHAJANA_ADMIN.BILLING_ADDRESS.BILLING_ADDRESS_ID%TYPE;
+    BEGIN
+        SELECT billing_address_id INTO v_address_id 
+        FROM SWADKHAJANA_ADMIN.BILLING_ADDRESS WHERE billing_address_id = p_address_id;
+        
+        -- Update the existing record
+        UPDATE SWADKHAJANA_ADMIN.BILLING_ADDRESS
+        SET ADDRESS_LINE_1 = p_line_1,
+            ADDRESS_LINE_2 = p_line_2,
+            CITY = p_city,
+            STATE = p_state,
+            PINCODE = p_pincode
+        WHERE BILLING_ADDRESS_ID = p_address_id;
+
+        DBMS_OUTPUT.PUT_LINE('Address updated');
+    EXCEPTION 
+        WHEN NO_DATA_FOUND THEN
+                -- Insert a new record if the address id is new
+                INSERT INTO SWADKHAJANA_ADMIN.BILLING_ADDRESS VALUES 
+                (p_address_id, p_cust_id, p_line_1, p_line_2,
+                    p_city, p_state, p_pincode);
+                DBMS_OUTPUT.PUT_LINE('Address added');
+    END;   
+    COMMIT; -- Commit the changes
+EXCEPTION
+    WHEN OTHERS THEN
+        IF SQLCODE = -2291 THEN
+            DBMS_OUTPUT.PUT_LINE('Invalid customer ID');
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('An error occurred: '|| SQLERRM);
+        END IF;
+        ROLLBACK; -- Rollback changes if an error occurs
+END Upsert_billing_address;
+END customer_pkg;
+/
+
+EXEC customer_pkg.Upsert_customer('Eva', 'Miller', 3339997777, 'davi.jones@email.com');
+EXEC customer_pkg.Upsert_customer('John', 'Doe', 1234567890, 'john.doe@email.com');
+EXEC customer_pkg.Upsert_customer('Jane', 'Doe', 2345678901, 'jane.doe@email.com');
+EXEC customer_pkg.Upsert_customer('Alice', 'Smith', 3456789012, 'alice.smith@email.com');
+EXEC customer_pkg.Upsert_customer('Bob', 'Smith', 4567890123, 'bob.smith@email.com');
+EXEC customer_pkg.Upsert_customer('David', 'Jones', 6789012345, 'david.jones@email.com');
+
+EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 1, '123 Main St', 'Apt 1', 'CityA', 'StateA', '12345');
+EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 2, '456 Elm St', 'Apt 2', 'CityB', 'StateB', '23456');
+EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 3, '789 Oak St', 'Apt 3', 'CityC', 'StateC', '34567');
+EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 4, '135 Pine St', 'Apt 4', 'CityD', 'StateD', '45678');
+EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 5, '246 Maple St', 'Apt 5', 'CityE', 'StateE', '56789');
+EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 6, '369 Birch St', 'Apt 6', 'CityF', 'StateF', '67890');
+EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.CURRVAL, 6, '369 Birch St', 'Apt 6', 'CityF', 'StateF', '67890');
+EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 7, '369 Birch St', 'Apt 6', 'CityF', 'StateF', '67890');
+
+EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 1, '321 Main St', 'Unit 1', 'CityA', 'StateA', '12345');
+EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 2, '654 Elm St', 'Unit 2', 'CityB', 'StateB', '23456');
+EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 3, '987 Oak St', 'Unit 3', 'CityC', 'StateC', '34567');
+EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 4, '531 Pine St', 'Unit 4', 'CityD', 'StateD', '45678');
+EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 5, '642 Maple St', 'Unit 5', 'CityE', 'StateE', '56789');
+EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 6, '963 Birch St', 'Unit 6', 'CityF', 'StateF', '67891');
+EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.CURRVAL, 6, '963 Birch St', 'Unit 6', 'CityF', 'StateF', '67890');
+EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 7, '963 Birch St', 'Unit 6', 'CityF', 'StateF', '67890');
+--SELECT * FROM SWADKHAJANA_ADMIN.CUSTOMER_DETAILS;
