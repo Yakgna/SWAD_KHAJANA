d145767c49efa0840fa23b035ddbb0f5b68d7cce
Vinay Sathe <144943447+VinaySathe-NEU@users.noreply.github.com>
2023-12-01 17:58:23 -0500

    Updated Customer_Package.sql

--- CUSTOMER_Package.sql
+++ CUSTOMER_Package.sql
@@ -247,30 +247,35 @@ END add_ordered_items;
 END customer_pkg;
 /
 
-EXEC customer_pkg.Upsert_customer('Eva', 'Miller', 3339997777, 'davi.jones@email.com');
-EXEC customer_pkg.Upsert_customer('John', 'Doe', 1234567890, 'john.doe@email.com');
-EXEC customer_pkg.Upsert_customer('Jane', 'Doe', 2345678901, 'jane.doe@email.com');
-EXEC customer_pkg.Upsert_customer('Alice', 'Smith', 3456789012, 'alice.smith@email.com');
-EXEC customer_pkg.Upsert_customer('Bob', 'Smith', 4567890123, 'bob.smith@email.com');
-EXEC customer_pkg.Upsert_customer('David', 'Jones', 6789012345, 'david.jones@email.com');
+EXEC customer_pkg.Upsert_customer('Eva', 'Miller', 3339997777, 'davi.jones@gmail.com');
+EXEC customer_pkg.Upsert_customer('John', 'Doe', 1234567890, 'john.doe@gmail.com');
+EXEC customer_pkg.Upsert_customer('Jane', 'Doe', 2345678901, 'jane.doe@gmail.com');
+EXEC customer_pkg.Upsert_customer('Alice', 'Smith', 3456789012, 'alice.smith@gmail.com');
+EXEC customer_pkg.Upsert_customer('Bob', 'Smith', 4567890123, 'bob.smith@gmail.com');
+EXEC customer_pkg.Upsert_customer('David', 'Jones', 6789012345, 'david.jones@gmail.com');
 
-EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 1, '123 Main St', 'Apt 1', 'CityA', 'StateA', '12345');
-EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 2, '456 Elm St', 'Apt 2', 'CityB', 'StateB', '23456');
-EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 3, '789 Oak St', 'Apt 3', 'CityC', 'StateC', '34567');
-EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 4, '135 Pine St', 'Apt 4', 'CityD', 'StateD', '45678');
-EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 5, '246 Maple St', 'Apt 5', 'CityE', 'StateE', '56789');
-EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 6, '369 Birch St', 'Apt 6', 'CityF', 'StateF', '67890');
-EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.CURRVAL, 6, '369 Birch St', 'Apt 6', 'CityF', 'StateF', '67890');
-EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 7, '369 Birch St', 'Apt 6', 'CityF', 'StateF', '67890');
+-- Adding delivery addresses for Massachusetts and New York
+-- Delivery addresses with real names for Massachusetts and New York
+EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 1, '123 Main St', 'Apt 1', 'Boston', 'Massachusetts', '02108');
+EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 2, '456 Elm St', 'Apt 2', 'New York City', 'New York', '10001');
+EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 3, '789 Oak St', 'Apt 3', 'Cambridge', 'Massachusetts', '02139');
+EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 4, '135 Pine St', 'Apt 4', 'Albany', 'New York', '12203');
+EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 5, '246 Maple St', 'Apt 5', 'Worcester', 'Massachusetts', '01608');
+EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.NEXTVAL, 6, '369 Birch St', 'Apt 6', 'Syracuse', 'New York', '13202');
+EXEC customer_pkg.Upsert_delivery_address('DA'||SWADKHAJANA_ADMIN.DELIVERY_ADDRESS_SEQ.CURRVAL, 7, '369 Birch St', 'Apt 6', 'Syracuse', 'New York', '13202');
 
-EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 1, '321 Main St', 'Unit 1', 'CityA', 'StateA', '12345');
-EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 2, '654 Elm St', 'Unit 2', 'CityB', 'StateB', '23456');
-EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 3, '987 Oak St', 'Unit 3', 'CityC', 'StateC', '34567');
-EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 4, '531 Pine St', 'Unit 4', 'CityD', 'StateD', '45678');
-EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 5, '642 Maple St', 'Unit 5', 'CityE', 'StateE', '56789');
-EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 6, '963 Birch St', 'Unit 6', 'CityF', 'StateF', '67891');
-EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.CURRVAL, 6, '963 Birch St', 'Unit 6', 'CityF', 'StateF', '67890');
-EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 7, '963 Birch St', 'Unit 6', 'CityF', 'StateF', '67890');
+
+-- Adding billing addresses for Massachusetts and New York
+-- Billing addresses with real names for Massachusetts and New York
+EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 1, '123 Main St', 'Apt 1', 'Boston', 'Massachusetts', '02108');
+EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 2, '456 Elm St', 'Apt 2', 'New York City', 'New York', '10001');
+EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 3, '789 Oak St', 'Apt 3', 'Cambridge', 'Massachusetts', '02139');
+EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 4, '135 Pine St', 'Apt 4', 'Albany', 'New York', '12203');
+EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 5, '246 Maple St', 'Apt 5', 'Worcester', 'Massachusetts', '01608');
+EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.NEXTVAL, 6, '963 Birch St', 'Unit 6', 'Syracuse', 'New York', '13202');
+EXEC customer_pkg.Upsert_billing_address('BA'||SWADKHAJANA_ADMIN.BILLING_ADDRESS_SEQ.CURRVAL, 7, '963 Birch St', 'Unit 6', 'Syracuse', 'New York', '13202');
+
+
 
 --Place order 1
 EXEC customer_pkg.place_order('O1', 'DA1', 'BA1', 'RBA1',  'RP1', 'P1', SYSDATE, 'DE1', 18);
@@ -279,7 +284,19 @@ EXEC customer_pkg.add_ordered_items('I2',4);
 EXEC customer_pkg.add_ordered_items('I3',1);
 
 --Place order 2
-EXEC customer_pkg.place_order('O2', 'DA2', 'BA2', 'RBA3',  NULL, 'P1', SYSDATE, 'DE2', 18);
+EXEC customer_pkg.place_order('O2', 'DA2', 'BA2', 'RBA3',  NULL, 'P2', SYSDATE, 'DE2', 18);
+EXEC customer_pkg.add_ordered_items('I1',2);
+EXEC customer_pkg.add_ordered_items('I2',4);
+EXEC customer_pkg.add_ordered_items('I3',1);
+
+--Place order 3
+EXEC customer_pkg.place_order('O3', 'DA5', 'BA5', 'RBA6',  'RP3', 'P3', SYSDATE, 'DE3', 18);
+EXEC customer_pkg.add_ordered_items('I1',2);
+EXEC customer_pkg.add_ordered_items('I2',4);
+EXEC customer_pkg.add_ordered_items('I3',1);
+
+--Place order 4
+EXEC customer_pkg.place_order('O4', 'DA7', 'BA7', 'RBA4',  'RP5', 'P4', SYSDATE, 'DE4', 18);
 EXEC customer_pkg.add_ordered_items('I1',2);
 EXEC customer_pkg.add_ordered_items('I2',4);
 EXEC customer_pkg.add_ordered_items('I3',1);
