c00005eb85d0f37528d89183def00293dcd166af
Yakgna <ramasamy.y@northeastern.edu>
2023-12-01 12:55:48 -0500

    1. Removed Insert statements for tables 2. Deleted Order_status_procedure.sql, Order_type_procedure.sql and Payment_procedure.sql

--- Create_Tables_Seq.sql
+++ Create_Tables_Seq.sql
@@ -95,7 +95,7 @@ CREATE TABLE order_status (
 
 CREATE TABLE restaurant (
     restaurant_id   VARCHAR(15),
-    restaurant_name VARCHAR(50) NOT NULL,
+    restaurant_name VARCHAR(50) UNIQUE NOT NULL,
     opening_time    DATE NOT NULL,
     closing_time    DATE NOT NULL,
     PRIMARY KEY ( restaurant_id )
@@ -122,7 +122,8 @@ CREATE TABLE branch_address (
     pincode           VARCHAR(10) NOT NULL,
     PRIMARY KEY ( branch_address_id ),
     CONSTRAINT "FK_Branch_address.restaurant_id" FOREIGN KEY ( restaurant_id )
-        REFERENCES restaurant ( restaurant_id )
+        REFERENCES restaurant ( restaurant_id ),
+    CONSTRAINT unique_address UNIQUE (address_line_1, address_line_2, city, state, pincode)
 );
 
 CREATE TABLE order_type (
@@ -252,38 +253,13 @@ CREATE SEQUENCE order_details_seq START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE order_items_seq START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE customer_details_seq START WITH 1 INCREMENT BY 1;
 
-
+ 
 /*
 ------------------------------------------------------------------------------------
 ---------------------------------- Insert data -------------------------------------
 ------------------------------------------------------------------------------------
 */
---Customer_details
-INSERT INTO Customer_details VALUES (CUSTOMER_DETAILS_SEQ.NEXTVAL, 'John', 'Doe', 1234567890, 'john.doe@email.com');
-INSERT INTO Customer_details VALUES (CUSTOMER_DETAILS_SEQ.NEXTVAL, 'Jane', 'Doe', 2345678901, 'jane.doe@email.com');
-INSERT INTO Customer_details VALUES (CUSTOMER_DETAILS_SEQ.NEXTVAL, 'Alice', 'Smith', 3456789012, 'alice.smith@email.com');
-INSERT INTO Customer_details VALUES (CUSTOMER_DETAILS_SEQ.NEXTVAL, 'Bob', 'Smith', 4567890123, 'bob.smith@email.com');
-INSERT INTO Customer_details VALUES (CUSTOMER_DETAILS_SEQ.NEXTVAL, 'Carol', 'Jones', 5678901234, 'carol.jones@email.com');
-INSERT INTO Customer_details VALUES (CUSTOMER_DETAILS_SEQ.NEXTVAL, 'David', 'Jones', 6789012345, 'david.jones@email.com');
-
---Delivery_address
-INSERT INTO Delivery_address VALUES ('DA'||DELIVERY_ADDRESS_SEQ.NEXTVAL, 1, '123 Main St', 'Apt 1', 'CityA', 'StateA', '12345');
-INSERT INTO Delivery_address VALUES ('DA'||DELIVERY_ADDRESS_SEQ.NEXTVAL, 2, '456 Elm St', 'Apt 2', 'CityB', 'StateB', '23456');
-INSERT INTO Delivery_address VALUES ('DA'||DELIVERY_ADDRESS_SEQ.NEXTVAL, 3, '789 Oak St', 'Apt 3', 'CityC', 'StateC', '34567');
-INSERT INTO Delivery_address VALUES ('DA'||DELIVERY_ADDRESS_SEQ.NEXTVAL, 4, '135 Pine St', 'Apt 4', 'CityD', 'StateD', '45678');
-INSERT INTO Delivery_address VALUES ('DA'||DELIVERY_ADDRESS_SEQ.NEXTVAL, 5, '246 Maple St', 'Apt 5', 'CityE', 'StateE', '56789');
-INSERT INTO Delivery_address VALUES ('DA'||DELIVERY_ADDRESS_SEQ.NEXTVAL, 6, '369 Birch St', 'Apt 6', 'CityF', 'StateF', '67890');
-
---Billing_address
-INSERT INTO Billing_address VALUES ('BA'||BILLING_ADDRESS_SEQ.NEXTVAL, 1, '321 Main St', 'Unit 1', 'CityA', 'StateA', '12345');
-INSERT INTO Billing_address VALUES ('BA'||BILLING_ADDRESS_SEQ.NEXTVAL, 2, '654 Elm St', 'Unit 2', 'CityB', 'StateB', '23456');
-INSERT INTO Billing_address VALUES ('BA'||BILLING_ADDRESS_SEQ.NEXTVAL, 3, '987 Oak St', 'Unit 3', 'CityC', 'StateC', '34567');
-INSERT INTO Billing_address VALUES ('BA'||BILLING_ADDRESS_SEQ.NEXTVAL, 4, '531 Pine St', 'Unit 4', 'CityD', 'StateD', '45678');
-INSERT INTO Billing_address VALUES ('BA'||BILLING_ADDRESS_SEQ.NEXTVAL, 5, '642 Maple St', 'Unit 5', 'CityE', 'StateE', '56789');
-INSERT INTO Billing_address VALUES ('BA'||BILLING_ADDRESS_SEQ.NEXTVAL, 6, '963 Birch St', 'Unit 6', 'CityF', 'StateF', '67890');
-
-
---Payment
+----Payment
 INSERT INTO Payment VALUES ('P'||PAYMENT_SEQ.NEXTVAL, 'Credit Card');
 INSERT INTO Payment VALUES ('P'||PAYMENT_SEQ.NEXTVAL, 'Debit Card');
 INSERT INTO Payment VALUES ('P'||PAYMENT_SEQ.NEXTVAL, 'Cash');
@@ -291,7 +267,8 @@ INSERT INTO Payment VALUES ('P'||PAYMENT_SEQ.NEXTVAL, 'Check');
 INSERT INTO Payment VALUES ('P'||PAYMENT_SEQ.NEXTVAL, 'Online Transfer');
 INSERT INTO Payment VALUES ('P'||PAYMENT_SEQ.NEXTVAL, 'Mobile Payment');
 
---Order_status
+----Order_status
+INSERT INTO Order_status VALUES ('OS'||ORDER_STATUS_SEQ.nextval, 'Placed');
 INSERT INTO Order_status VALUES ('OS'||ORDER_STATUS_SEQ.nextval, 'Received');
 INSERT INTO Order_status VALUES ('OS'||ORDER_STATUS_SEQ.nextval, 'Processing');
 INSERT INTO Order_status VALUES ('OS'||ORDER_STATUS_SEQ.nextval, 'Ready');
@@ -299,104 +276,8 @@ INSERT INTO Order_status VALUES ('OS'||ORDER_STATUS_SEQ.nextval, 'Dispatched');
 INSERT INTO Order_status VALUES ('OS'||ORDER_STATUS_SEQ.nextval, 'Delivered');
 INSERT INTO Order_status VALUES ('OS'||ORDER_STATUS_SEQ.nextval, 'Cancelled');
 
---Restaurant
-INSERT INTO Restaurant VALUES ('R'||RESTAURANT_SEQ.NEXTVAL, 'Burger Hub', TO_DATE('10:00:00', 'HH24:MI:SS'), TO_DATE('23:59:59', 'HH24:MI:SS'));
-INSERT INTO Restaurant VALUES ('R'||RESTAURANT_SEQ.NEXTVAL, 'Seafood Delight', TO_DATE('11:00:00', 'HH24:MI:SS'), TO_DATE('22:00:00', 'HH24:MI:SS'));
-INSERT INTO Restaurant VALUES ('R'||RESTAURANT_SEQ.NEXTVAL, 'Mountain View Cafe', TO_DATE('07:30:00', 'HH24:MI:SS'), TO_DATE('20:00:00', 'HH24:MI:SS'));
-INSERT INTO Restaurant VALUES ('R'||RESTAURANT_SEQ.NEXTVAL, 'Urban Eats', TO_DATE('09:00:00', 'HH24:MI:SS'), TO_DATE('23:30:00', 'HH24:MI:SS'));
-INSERT INTO Restaurant VALUES ('R'||RESTAURANT_SEQ.NEXTVAL, 'Garden Bistro', TO_DATE('08:00:00', 'HH24:MI:SS'), TO_DATE('21:30:00', 'HH24:MI:SS'));
-INSERT INTO Restaurant VALUES ('R'||RESTAURANT_SEQ.NEXTVAL, 'The Downtown Diner', TO_DATE('06:00:00', 'HH24:MI:SS'), TO_DATE('23:59:59', 'HH24:MI:SS'));
-
---Branch_address
-INSERT INTO Branch_address VALUES ('BA'||BRANCH_ADDRESS_SEQ.NEXTVAL, 'R1', 9876543210, '123 Burger St', 'Suite 101', 'CityG', 'StateG', '78901');
-INSERT INTO Branch_address VALUES ('BA'||BRANCH_ADDRESS_SEQ.NEXTVAL, 'R2', 8765432190, '456 Seafood Rd', 'Suite 102', 'CityH', 'StateH', '89012');
-INSERT INTO Branch_address VALUES ('BA'||BRANCH_ADDRESS_SEQ.NEXTVAL, 'R3', 7654321980, '789 Cafe Blvd', 'Suite 103', 'CityI', 'StateI', '90123');
-INSERT INTO Branch_address VALUES ('BA'||BRANCH_ADDRESS_SEQ.NEXTVAL, 'R3', 6543219870, '135 Urban Way', 'Suite 104', 'CityJ', 'StateJ', '01234');
-INSERT INTO Branch_address VALUES ('BA'||BRANCH_ADDRESS_SEQ.NEXTVAL, 'R4', 5432198760, '246 Bistro Ln', 'Suite 105', 'CityK', 'StateK', '12345');
-INSERT INTO Branch_address VALUES ('BA'||BRANCH_ADDRESS_SEQ.NEXTVAL, 'R5', 4321987650, '369 Diner Ave', 'Suite 106', 'CityL', 'StateL', '23456');
-
---Delivery_executive
-INSERT INTO Delivery_executive VALUES ('DE'||DELIVERY_EXECUTIVE_SEQ.NEXTVAL, 'Emma', 'Brown', TO_DATE('1990-01-15', 'YYYY-MM-DD'), 1122334455);
-INSERT INTO Delivery_executive VALUES ('DE'||DELIVERY_EXECUTIVE_SEQ.NEXTVAL, 'Liam', 'Johnson', TO_DATE('1988-05-22', 'YYYY-MM-DD'), 2233445566);
-INSERT INTO Delivery_executive VALUES ('DE'||DELIVERY_EXECUTIVE_SEQ.NEXTVAL, 'Olivia', 'Williams', TO_DATE('1992-07-30', 'YYYY-MM-DD'), 3344556677);
-INSERT INTO Delivery_executive VALUES ('DE'||DELIVERY_EXECUTIVE_SEQ.NEXTVAL, 'Noah', 'Davis', TO_DATE('1989-11-09', 'YYYY-MM-DD'), 4455667788);
-INSERT INTO Delivery_executive VALUES ('DE'||DELIVERY_EXECUTIVE_SEQ.NEXTVAL, 'Ava', 'Miller', TO_DATE('1991-04-17', 'YYYY-MM-DD'), 5566778899);
-INSERT INTO Delivery_executive VALUES ('DE'||DELIVERY_EXECUTIVE_SEQ.NEXTVAL, 'William', 'Wilson', TO_DATE('1987-02-25', 'YYYY-MM-DD'), 6677889900);
-
---Promo_codes
-INSERT INTO Promo_codes VALUES ('PR'||PROMO_CODES_SEQ.NEXTVAL, 10);
-INSERT INTO Promo_codes VALUES ('PR'||PROMO_CODES_SEQ.NEXTVAL, 15);
-INSERT INTO Promo_codes VALUES ('PR'||PROMO_CODES_SEQ.NEXTVAL, 20);
-INSERT INTO Promo_codes VALUES ('PR'||PROMO_CODES_SEQ.NEXTVAL, 5);
-INSERT INTO Promo_codes VALUES ('PR'||PROMO_CODES_SEQ.NEXTVAL, 25);
-INSERT INTO Promo_codes VALUES ('PR'||PROMO_CODES_SEQ.NEXTVAL, 30);
-
---Restaurant_promo relation
-INSERT INTO Restaurant_promo VALUES ('RP'||RESTAURANT_PROMO_SEQ.NEXTVAL, 'PR1', 'R1');
-INSERT INTO Restaurant_promo VALUES ('RP'||RESTAURANT_PROMO_SEQ.NEXTVAL, 'PR2', 'R2');
-INSERT INTO Restaurant_promo VALUES ('RP'||RESTAURANT_PROMO_SEQ.NEXTVAL, 'PR3', 'R3');
-INSERT INTO Restaurant_promo VALUES ('RP'||RESTAURANT_PROMO_SEQ.NEXTVAL, 'PR4', 'R4');
-INSERT INTO Restaurant_promo VALUES ('RP'||RESTAURANT_PROMO_SEQ.NEXTVAL, 'PR5', 'R5');
-INSERT INTO Restaurant_promo VALUES ('RP'||RESTAURANT_PROMO_SEQ.NEXTVAL, 'PR6', 'R6');
-
--- Items
--- Add items for Restaurant R1
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R1', 'Chicken Sandwich', 6.99);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R1', 'Caesar Salad', 8.99);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R1', 'French Fries', 3.99);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R1', 'Chocolate Shake', 4.99);
-
--- Add items for Restaurant R2
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R2', 'Margherita Pizza', 10.99);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R2', 'Pasta Alfredo', 7.99);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R2', 'Garlic Bread', 4.50);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R2', 'Tiramisu', 6.99);
-
--- Add items for Restaurant R3
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R3', 'BBQ Ribs', 15.99);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R3', 'Shrimp Scampi', 11.99);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R3', 'Mashed Potatoes', 3.99);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R3', 'Cheesecake', 5.99);
-
--- Add items for Restaurant R4
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R4', 'Steak Fajitas', 13.99);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R4', 'Chicken Quesadilla', 8.99);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R4', 'Guacamole', 2.99);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R4', 'Salsa and Chips', 3.50);
-
--- Add items for Restaurant R5
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R5', 'Espresso', 2.99);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R5', 'Chocolate Croissant', 3.99);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R5', 'Fruit Salad', 4.50);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R5', 'Iced Latte', 5.99);
-
--- Add items for Restaurant R6
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R6', 'Avocado Toast', 5.99);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R6', 'Fruit Smoothie', 4.50);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R6', 'Vegetarian Wrap', 6.99);
-INSERT INTO Items VALUES ('I'||ITEMS_SEQ.NEXTVAL, 'R6', 'Green Tea', 2.99);
-
-
--- Order_type
+---- Order_type
 INSERT INTO Order_type VALUES ('O'||ORDER_TYPE_SEQ.NEXTVAL, 'Pickup');
 INSERT INTO Order_type VALUES ('O'||ORDER_TYPE_SEQ.NEXTVAL, 'Delivery');
 INSERT INTO Order_type VALUES ('O'||ORDER_TYPE_SEQ.NEXTVAL, 'Drive-Thru');
 INSERT INTO Order_type VALUES ('O'||ORDER_TYPE_SEQ.NEXTVAL, 'Inter-state');
-
-
--- Order_Details
-INSERT INTO Order_details VALUES ('OD'||ORDER_DETAILS_SEQ.NEXTVAL, 'O1', 'BA1', 'OS1', 'DE1', 'DA1', 'BA1', 'RP1', 'P1', SYSDATE, 18);
-INSERT INTO Order_details VALUES ('OD'||ORDER_DETAILS_SEQ.NEXTVAL, 'O2', 'BA2', 'OS2', 'DE2', 'DA2', 'BA2', 'RP2', 'P2', SYSDATE, 18);
-INSERT INTO Order_details VALUES ('OD'||ORDER_DETAILS_SEQ.NEXTVAL, 'O3', 'BA3', 'OS3', 'DE3', 'DA3', 'BA3', 'RP3', 'P3', SYSDATE, 18);
-INSERT INTO Order_details VALUES ('OD'||ORDER_DETAILS_SEQ.NEXTVAL, 'O1', 'BA4', 'OS4', 'DE4', 'DA4', 'BA4', 'RP4', 'P4', SYSDATE, 18);
-INSERT INTO Order_details VALUES ('OD'||ORDER_DETAILS_SEQ.NEXTVAL, 'O2', 'BA5', 'OS5', 'DE5', 'DA5', 'BA5', 'RP5', 'P5', SYSDATE, 18);
-INSERT INTO Order_details VALUES ('OD'||ORDER_DETAILS_SEQ.NEXTVAL, 'O3', 'BA6', 'OS6', 'DE6', 'DA6', 'BA6', 'RP6', 'P6', SYSDATE, 18);
-
-
--- Order_Items
-INSERT INTO Ordered_items VALUES ('I1', 'OD1', 2);
-INSERT INTO Ordered_items VALUES ('I2', 'OD1', 1);
-INSERT INTO Ordered_items VALUES ('I3', 'OD2', 3);
-INSERT INTO Ordered_items VALUES ('I4', 'OD3', 1);
-INSERT INTO Ordered_items VALUES ('I5', 'OD4', 2);
-INSERT INTO Ordered_items VALUES ('I6', 'OD5', 1);
